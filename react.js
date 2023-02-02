a66e984522862a7ec0714d6cedc8d5d06aadc78e
Jefté Medeiros <jefte.medeiros8@gmail.com>
2023-02-02 18:45:20 -0500

    react-config

---
+++ react.js
@@ -0,0 +1,40 @@
+module.exports= {
+  env: {
+    browser: true
+  },
+  extends: [
+    './index.js',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+  },
+  plugins: [
+    'react',
+    'jsx-a11y',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'jsx-a11y/alt-text': [
+      'warn',
+      {
+        elements: ['img'],
+        img: ['Image'],
+      },
+    ],
+    'jsx-a11y/aria-props': 'warn',
+    'jsx-a11y/aria-proptypes': 'warn',
+    'jsx-a11y/aria-unsupported-elements': 'warn',
+    'jsx-a11y/role-has-required-aria-props': 'warn',
+    'jsx-a11y/role-supports-aria-props': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
